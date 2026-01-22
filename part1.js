8887a8e97fde89e4c1b7be9ff74fac47230a68f2
hashir-stack <hashir7ansari@gmail.com>
2026-01-22 15:54:14 +0530

    Type Coercion

--- part1.js
+++ part1.js
@@ -19,28 +19,42 @@
 // Check type: typeof(variable)
 
 // Case 1 :
-    // let a = 12;
-    // let b = "13";
-    // console.log(typrof(a + b)); // "1213" (string)
+// let a = 12;
+// let b = "13";
+// console.log(typrof(a + b)); // "1213" (string)
 
 // Case 2 :
-    // let a = 12;
-    // let b = 13;
-    // console.log(typrof(a + b)); //25 (number)
+// let a = 12;
+// let b = 13;
+// console.log(typrof(a + b)); //25 (number)
 
 // Case 3 : // String + Number + Number => String (concat)
-    // let a = 10;
-    // let b = 20;
-    // console.log("Sum of 10 and 20 is" + a + b); Output => Sum of 10 and 20 is 1020 (string)
+// let a = 10;
+// let b = 20;
+// console.log("Sum of 10 and 20 is" + a + b); Output => Sum of 10 and 20 is 1020 (string)
 
-// Case 4 : // 
-    // let a = 10;
-    // let b = 20;
-    // console.log("Sum of 10 and 20 is" + (a + b)); Output => Sum of 10 and 20 is 30 (string) BOARD mass rule (priority bracket ko milegi)
+// Case 4 : //
+// let a = 10;
+// let b = 20;
+// console.log("Sum of 10 and 20 is" + (a + b)); Output => Sum of 10 and 20 is 30 (string) BOARD mass rule (priority bracket ko milegi)
 
-// Case 3 : //  Number + Number + String => String (concat)
-    // let a = 10;
-    // let b = 20;
-    // console.log(a + b +" is Sum of 10 and 20"  ); Output => 30 is Sum of 10 and 20 (string)
+// Case 5 : //  Number + Number + String => String (concat)
+// let a = 10;
+// let b = 20;
+// console.log(a + b +" is Sum of 10 and 20"  ); Output => 30 is Sum of 10 and 20 (string)
+// ----------------------------------------------------------------------------------------------------------------------------------------
 
-    
+// 3. Type Coercion (JS Magic Conversion)
+// JS engine changes types based on operator is called Type Coercion.
+
+// + ==> If one side string → concat (text join)  Because + do kaam hai ek addtion krna aur ek concat krna
+// For Example :
+                // "1" + 1 = 11 Output
+
+// -, *, / ==> Convert string to number first.*  - ,*, /  ye sub operator ka sirf ek hi kaam hai isliye yesa ho raha hai
+// For Example :
+                // "1" - 1 = 0 Output
+                // "1" * 1 = 1 Output
+                // "1" / 1 = 1 Output
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
