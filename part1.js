5ae6675bd7d3346aae41d376ca578c42f6f78726
hashir-stack <hashir7ansari@gmail.com>
2026-01-22 15:34:23 +0530

    Updates

---
+++ part1.js
@@ -0,0 +1,46 @@
+// 1. Variables: var vs let
+
+// var => Can use before declaration (prints value, no error). Banane se phale use kr sakte hai undefined aa sakta hai error nhi aata
+// a = 12;
+// console.log(a);
+// var a;
+
+// let => Cannot use before declaration ("Cannot access before initialization").
+// b = 13;
+// console.log(b);
+// let b; //==> (throw reference error)
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+// 2. Numbers vs Strings (Key Difference)
+
+// Numbers: 10 + 1 = 11 (addition).  Adding number to number is called Addition.
+// Strings: "10" + 1 = "101" (concatenation, joins text). Adding number to string is called Concatenation.
+// Console colors: Blue = number, black = string. (For the Tips Purpose only)
+// Check type: typeof(variable)
+
+// Case 1 :
+    // let a = 12;
+    // let b = "13";
+    // console.log(typrof(a + b)); // "1213" (string)
+
+// Case 2 :
+    // let a = 12;
+    // let b = 13;
+    // console.log(typrof(a + b)); //25 (number)
+
+// Case 3 : // String + Number + Number => String (concat)
+    // let a = 10;
+    // let b = 20;
+    // console.log("Sum of 10 and 20 is" + a + b); Output => Sum of 10 and 20 is 1020 (string)
+
+// Case 4 : // 
+    // let a = 10;
+    // let b = 20;
+    // console.log("Sum of 10 and 20 is" + (a + b)); Output => Sum of 10 and 20 is 30 (string) BOARD mass rule (priority bracket ko milegi)
+
+// Case 3 : //  Number + Number + String => String (concat)
+    // let a = 10;
+    // let b = 20;
+    // console.log(a + b +" is Sum of 10 and 20"  ); Output => 30 is Sum of 10 and 20 (string)
+
+    
