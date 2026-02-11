e8f672ba0df45704bc99ac3674aff6d95b16959e
hashir-stack <hashir7ansari@gmail.com>
2026-02-11 18:02:46 +0530

    Recursion Introduction and examples

---
+++ recursion.js
@@ -0,0 +1,20 @@
+//  Recursion :-
+
+// Recursion in JavaScript is a technique where a function calls itself until a base case is met. It’s often used for problems that can be broken down into smaller, similar subproblems, such as factorials, Fibonacci sequences, or tree traversal.
+//                                                          or
+
+// Function calls itself repeatedly until base case (stopping point). 
+
+// Recursion Examples (JavaScript Code)
+
+// 1. Print "Hello World" n Times
+
+// function temp(n) {
+//   if (n === 0) return;  // Base case
+//   console.log("Hello World");
+//   temp(n - 1);  // Recursive call
+// }
+// temp(5);
+// Prints during forward pass (5 pushes, then pops).
+//----------------------------------------------------------------------------------------------------------------------------------
+
