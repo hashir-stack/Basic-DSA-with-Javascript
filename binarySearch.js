abfe483b92b737a5f3bc8e96ff2c99e380b8df7b
hashir-stack <hashir7ansari@gmail.com>
2026-02-14 18:03:13 +0530

    Binary Search

---
+++ binarySearch.js
@@ -0,0 +1,5 @@
+// Binary Search Algorithm :-
+
+// Binary search is the fastest searching algorithm, much better than linear search. It works only on sorted data (ascending or descending order).  
+
+// Key requirement: Data must be sorted. Even if data looks unsorted in some cases, specific situations allow binary search (explained later).
