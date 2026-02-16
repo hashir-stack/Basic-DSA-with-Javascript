270dd1181da9b7acdf2883d907cd48ed25212dbe
hashir-stack <hashir7ansari@gmail.com>
2026-02-16 15:56:08 +0530

    Merge Sort Algo Introduction

---
+++ mergeSort.js
@@ -0,0 +1,10 @@
+// Merge Sort Notes: Divide and Conquer Algorithm (JavaScript Implementation)
+  
+// Core Concept: Merge Sort is a divide and conquer algorithm that breaks down an array into smaller sorted partitions, then merges them back into a fully sorted array.   It's efficient and stable, favored in coding interviews
+
+// How It Works
+// - Divide: Recursively split the array into halves until each subarray has one element.
+// - Conquer: Merge the subarrays back together in sorted order.
+// - Combine: The merge step ensures the final array is sorted
+
+// --------------------------------------------------------------------------------------------------------------
